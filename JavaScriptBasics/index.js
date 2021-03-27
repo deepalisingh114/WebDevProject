8f4c24ef942d2d40d330f42f3bb8779464d6c1f2
deepalisingh114 <deepalisingh114@gmail.com>
2021-03-28 04:24:21 +0530

    Java Script Basics-Variables,Data types, operators,funtions ,loops

--- JavaScriptBasics/index.js
+++ JavaScriptBasics/index.js
@@ -1,2 +1,135 @@
+ //1.ways to print in JavaScript
+        //console.log("Hello World!")
+        //alert('me')
+       // document.write("This is Document Write")
+//2.Console API
+       //console.log("Hello World!")
+      // console.warn("This is a Warning")
+      // console.error('This is an earror')
+//3. JavaScript Variables
+    //What are variable?- Containers to store values
+    // var number1 =34;
+    // var number2 = 56;
+    // console.log(number1 + number2);
+//4.Data Types in JS
+    //number 
+        // var num1 = 33;
+        // var num2 = 33.33;
+    //string
+        // var str1 = 'This is a string';
+        // var str2 = "This is also a string";
+    
+    //booleans
+        // var a = true;
+        // var b = false;
+        // var und;  
+        // var n = null;
+    // console.log(n)
+// At a ver high lever, there are two types of data types:
+// 1.Primitives data types:undefined, null,number,string,boolean , symbol
+// 2.Reference data type:Arrays and objects
+    //Array
+        // var arr = [1,2,3,4,5]
+        // console.log(arr)
+    //Objects
+        // var marks ={
+        // ravi: 24,
+        // Shubham:45,
+        // Deepali:98
+        // }   
+//Operators in JS
+    //1.Arithmetic operators + , -, /,*
+        // var x =20;
+        // var y= 10;
+        /*
+        console.log("The sum of these numbers is x+y",x+y)
+        console.log("The substraction of these numbers is x-y",x-y)
+        console.log("The multiplication of these numbers is x*y",x*y)
+        console.log("The division of these numbers is x/y",x/y)
+        */
+    //2.Assignment Operators
+        // var z = y;
+        // y+= 2;
+    //3.Comparison Operators
+        //== true is equal false if unequal
+        //>= true or false
+        //<= true or false
+        //>  greater tha true or false
+        //< smaller than true or false 
 
-console.log('Hello World'); 
+    //4.Logical Operators-operate in boolean
+        //1  Logical and i.e. &&
+             //true&&true  gives true
+            //true&&false   gives false
+            //false&&false  gives false
+
+         //2.  Logical OR i.e. || or means if any one true it gives true 
+            //true||true  gives true
+            //true||false   gives true
+            //false||false  gives false
+        //3 Logical not
+            //!false=true
+            //!true=false
+//Functions in JS
+
+ function avg(a,b){
+     c= (a+b)/2;
+     return c;
+ }
+
+ c1 = avg(9,3);
+ c2 =avg(4,9);
+//  console.log(c1 + c2);
+
+
+//conditionals in JS
+var m =1;
+//Single if statement
+// if (m>=18){
+//     console.log('your are an adult')
+// }
+// //If-else Statement
+// if (m>=60){
+//     console.log('your are old')
+// else{
+//     console.log('your are not an adult')
+// }
+//if-else ladder
+// if (m>=60){
+//     console.log('your are old')
+// }
+// else if (m>=18){
+//     console.log('your are young')
+// }
+// else if(m>=13){
+//     console.log( 'You are a teenager')
+// }
+// else if (m>=2){
+//     console.log('your are a kid')
+// }
+// else {
+//     console.log('you are a baby')
+// }
+var arr = [1,2,3,4,5,6,7];
+arr.forEach(function(element){
+    console.log(element+1);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
